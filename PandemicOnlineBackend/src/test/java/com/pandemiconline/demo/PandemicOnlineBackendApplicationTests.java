79947c7eee2782035a2f4c644dbc6f94c1699f91
Ikbendennis <58038071+Ikbendennis@users.noreply.github.com>
2021-05-18 10:06:03 +0200

    Add files via upload

---
+++ PandemicOnlineBackend/src/test/java/com/pandemiconline/demo/PandemicOnlineBackendApplicationTests.java
@@ -0,0 +1,15 @@
+package com.pandemiconline.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PandemicOnlineBackendApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
